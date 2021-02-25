e6bf35ffed577d3a5df136f75044989eeb22f1a2
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2021-02-25 11:45:27 +0100

    test code split

--- pioupiou.py
+++ pioupiou.py
@@ -8,18 +8,20 @@ import numpy.random as npr
 import scipy.special as ss
 import wrapt
 
-class Universe: # actually, looks like a random VECTOR, the only one in town so far.
-    def __init__(self, seed=None, n=0):
+class UniverseType: # actually, looks like a random VECTOR, the only one in town so far.
+    def __init__(self, seed=0, n=0):
         self.seed = seed
         self.rng = npr.default_rng(self.seed)
         self.n = 0
+    def restart(self):
+        self.__init__(self.seed)
     def __call__(self, u=None):
         if u is not None:
             return u
         else:
             return self.rng.uniform(size=self.n)
 
-U = Universe() # the universe (as long as we sample the variables only once ;
+Universe = UniverseType() # the universe (as long as we sample the variables only once ;
 # otherwise the "true" univers is the cartesian product of this one).
 # Here U is the "universal" random vector, that sums up everything there is
 # to know about the universe.
@@ -117,8 +119,8 @@ class Constant(RandomVariable):
 
 class Uniform(RandomVariable):
     def __init__(self, low=0.0, high=1.0):
-        self.n = U.n
-        U.n += 1
+        self.n = Universe.n
+        Universe.n += 1
         if not isinstance(low, RandomVariable):
             low = Constant(low)
         if not isinstance(high, RandomVariable):
@@ -127,13 +129,13 @@ class Uniform(RandomVariable):
         self.high = high
     def __call__(self, u=None):
         if u is None:
-            u = U()
+            u = Universe()
         u_n = u[self.n]
         return self.low(u) * (1 - u_n) + self.high(u) * u_n
 
 class Normal(RandomVariable):
     def __init__(self, mu=0.0, sigma=1.0):
-        self.U = Uniform()
+        self.U1 = Uniform()
         if not isinstance(mu, RandomVariable):
             mu = Constant(mu)
         if not isinstance(sigma, RandomVariable):
@@ -141,8 +143,8 @@ class Normal(RandomVariable):
         self.mu = mu
         self.sigma = sigma
     def __call__(self, u=None):
-        u = self.U(u) # arf, here the abstraction of implementation if U leaks through
-        return ss.erfinv(2*u - 1) * np.sqrt(2) * self.sigma(u) + self.mu(u)
+        u1 = self.U1(u)
+        return ss.erfinv(2*u1 - 1) * np.sqrt(2) * self.sigma(u1) + self.mu(u1)
 
 @wrapt.decorator
 def function(wrapped, instance, args, kwargs):
@@ -158,86 +160,13 @@ def function(wrapped, instance, args, kwargs):
             self.args = [randomize(arg) for arg in args]
             self.kwargs = {k: randomize(v) for k, v in kwargs.items()}
         def __call__(self, u=None):
-            u = U(u) # manage u = None
+            u = Universe(u) # manage u = None
             args_values = [arg(u) for arg in self.args]
             kwargs_values = {k: v(u) for k, v in kwargs.items()}
             return wrapped(*args_values, **kwargs_values)
     return Deterministic(*args, **kwargs)
 
-# TODO: support a decorator that transform a function into random var when
-# random vars are "plugged" into it. Then use it to patch most numpy functions ?
 for name in dir(np):
     item = getattr(np, name)
     if isinstance(item, np.ufunc):
         globals()[name] = function(item)
-
-# TODO: support operations on random variables : +, -, *, etc.
-
-# @function
-# def __add__(x, y):
-#     return x + y
-# RandomVariable.__add__ = __add__
-
-# ------------------------------------------------------------------------------
-
-U1 = Uniform(0.0, 1.0)
-for n in range(10):
-    print(U1())
-
-print(40*"-")
-U2 = Uniform(0.0, U1)
-for n in range(10):
-    print(U2())
-
-print(40*"-")
-g = Normal(1.5, 2.7)
-s = [g() for i in range(10000)]
-print("mean:", np.mean(s))    
-print("std dev:", np.std(s))
-
-print(40*"-")
-print("dimension of the universe:", U.n)
-
-print(40*"-")
-@function
-def add(x, y):
-    return x + y
-X = add(U1, U2)
-print([X() for i in range(10)])
-
-@function
-def substract(x, y):
-    return x - y
-Z = substract(X, X)
-print([Z() for i in range(10)])
-
-print(40*"-")
-print(exp(1.0))
-N1 = Normal(1.0, 0.001)
-X = exp(N1)
-print([X() for i in range(10)])
-
-print(40*"-")
-N1 = Normal(1.0, 0.01)
-N2 = Normal(2.0, 0.02)
-X = N1 + N2
-#print(X.__add__)
-#print(inspect.signature(X.__add__))
-print([X() for i in range(10)])
-
-
-print(40*"-")
-U1 = Uniform()
-X = U1 + 1.0
-Y = 1.0 + U1
-Z = X * Y
-#print(X.__add__)
-#print(inspect.signature(X.__add__))
-print([X() for i in range(10)])
-print([Y() for i in range(10)])
-print([Z() for i in range(10)])
-
-print(40*"-")
-U1 = Uniform()
-B = U1 >= 1/3
-print([B() for i in range(20)])
