f2372f6eb1e74fd801390d7aa7ab55042bd71a87
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2021-02-27 12:58:36 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 options = dict(
     name = "boisgera-pioupiou",
-    version = "0.0a9",
+    version = "0.0a10",
     license = "MIT License",
     description = "A nano probabilistic programming language for Python",
     author = "Sébastien Boisgérault",
