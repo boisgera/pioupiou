37024dd5eacf4461013a1eb09a3e57244bf459c5
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2021-03-01 20:49:59 +0100

    cleanup test.py

--- test.py
+++ test.py
@@ -30,25 +30,16 @@ fails = 0
 tests = 0
 for filename in test_files:
     options = {"module_relative": False, "verbose": verbose}
-
-    # Relax the tests to deal with test files that have a '\n' line break
-    # (Linux flavor) which does not match the pandoc line break on Windows 
-    # (Windows flavor : '\r\n').
-    # The proper way to deal with this would be to convert the test files 
-    # beforehand on Windows.
-    if platform.system() == "Windows": 
-        options["optionflags"] = doctest.NORMALIZE_WHITESPACE
-
     _fails, _tests = doctest.testfile(filename, **options)
     fails += _fails
     tests += _tests
 
-if fails > 0 or verbose:
+if fails > 0 or verbose: # pragma: no cover
    print()
    print(60*"-")
    print("Test Suite Report:", end=" ")
    print("{0} failures / {1} tests".format(fails, tests))
    print(60*"-")
-if fails:
+if fails: # pragma: no cover
     sys.exit(1)
  
