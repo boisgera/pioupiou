9849106cba3eeb3d1dcc144580c7f1eacb5aff48
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2021-03-01 12:48:33 +0100

    build images in CI (gha)

--- images/build.py
+++ images/build.py
@@ -23,3 +23,29 @@ g = sns.jointplot(x="x", y="y", data=data,
 #                  ratio = 4/3) #height=7)
 
 plt.savefig("xy.svg")
+
+# ------------------------------------------------------------------------------
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import pioupiou as pp; pp.restart()
+
+proportion_female = 0.516
+Is_female = pp.Bernoulli(proportion_female)
+mu_female = 161.7
+sigma_female = (175.0 - 149.0) / (2 * 1.96)
+Height_female = pp.Normal(mu_female, sigma_female)
+mu_male = 174.4
+sigma_male = (189.0 - 162.0) / (2 * 1.96)
+Height_male = pp.Normal(mu_male, sigma_male)
+
+Is_male = pp.logical_not(Is_female)
+Height = Is_female * Height_female + Is_male * Height_male
+
+omega = pp.Omega(1000)
+
+height = Height(omega) # doctest: +NORMALIZE_WHITESPACE, +ELLIPSIS
+df = pd.DataFrame({"Height [cm]": height})
+sns.displot(df, x="Height [cm]", stat="density", kde=True, aspect=16/9)
+plt.savefig("height.svg")
