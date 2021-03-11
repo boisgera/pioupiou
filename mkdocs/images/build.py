a2b3ceaa2f75794ce1f46c06e11a0d6089edde1f
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2021-03-11 20:13:16 +0100

    fix mkdocs build

--- mkdocs/images/build.py
+++ mkdocs/images/build.py
@@ -8,11 +8,35 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import pioupiou as pp
 
+# TODO: use more from test.py (automate iteration on files to begin with)
+
 # Helpers
 # ------------------------------------------------------------------------------
 
 def fetch_code(src):
+
+    # TODO: refactor here, that's a cut-and-paste from test.py (test runner)
+    lines = src.splitlines()
+    code, prompt = False, False
+    for i, line in enumerate(lines):
+        if line.startswith("```python"):
+            code = True
+            prompt = lines[i+1].startswith(">>> ")
+            lines[i] = ""
+        elif line.startswith("```"):
+            code = False
+            lines[i] = ""
+        elif code == True:
+            if not prompt: 
+                if line.startswith(" "): # heuristic for line continuation
+                    lines[i] = "... " + line
+                else:
+                    lines[i] = ">>> " + line
+            lines[i] = 4 * " " + lines[i]
+    src = "\n".join(src)
+
     code = ""
+    fences = False
     for line in src.splitlines():
         line = line.strip()
         if line.startswith(">>> ") or line.startswith("... "):
@@ -32,15 +56,11 @@ def fetch_viz_code(src):
             add = False
     return code
         
-
-
 # ------------------------------------------------------------------------------
 
 src = open("../index.md").read()
 code = fetch_code(src)
 exec(code)
-viz_code = fetch_viz_code(src)
-exec(viz_code)
 plt.close("all")
 
 # Height
@@ -48,8 +68,6 @@ plt.close("all")
 src = open("../height.md").read()
 code = fetch_code(src)
 exec(code)
-viz_code = fetch_viz_code(src)
-exec(viz_code)
 plt.close("all")
 
 # Gaussians
@@ -57,14 +75,10 @@ plt.close("all")
 src = open("../gaussians.md").read()
 code = fetch_code(src)
 exec(code)
-viz_code = fetch_viz_code(src)
-exec(viz_code)
 plt.close("all")
 
 # ------------------------------------------------------------------------------
 src = open("../volatility.md").read()
 code = fetch_code(src)
 exec(code)
-viz_code = fetch_viz_code(src)
-exec(viz_code)
 plt.close("all")
